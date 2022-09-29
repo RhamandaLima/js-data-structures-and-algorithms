b216d51774bb3daf9e076f8e61c501a62f243f58
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-09-29 15:59:27 -0300

    feat: adding step 03 - variables

---
+++ examples/exampleOne/02-Variables.js
@@ -0,0 +1,16 @@
+var num = 1; 
+num = 3; 
+var price = 1.5; 
+var myName = 'Packt';
+var trueValue = true; 
+var nullVar = null; 
+var und;
+
+console.log('num: ' + num);
+console.log('myName: ' + myName);
+console.log('price: ' + price);
+console.log('trueVale: ' + trueValue);
+console.log('nullVar: ', nullVar);
+console.log('und: ', und);
+
+// Ao utilizar console.log('und: ', und) permite-se adicionar uma descrição e visualizar o conteúdo caso ela um objeto, enquanto console.log('und: ' + und) concatena o resultado em uma única string.
