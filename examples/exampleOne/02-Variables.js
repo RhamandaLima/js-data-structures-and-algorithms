1ad57c3c086f9995d3c6118c1bfd96d4e96a52fc
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-09-29 16:14:35 -0300

    feat: adding step 04 - scope of variables

--- examples/exampleOne/02-Variables.js
+++ examples/exampleOne/02-Variables.js
@@ -14,3 +14,24 @@ console.log('nullVar: ', nullVar);
 console.log('und: ', und);
 
 // Ao utilizar console.log('und: ', und) permite-se adicionar uma descrição e visualizar o conteúdo caso ela um objeto, enquanto console.log('und: ' + und) concatena o resultado em uma única string.
+
+// Escopo das variáveis:
+
+var myVariable = 'global';
+myOtherVariable = 'global';
+
+function myFunction() {
+    var myVariable = 'local';
+    return myVariable;
+}
+
+function myOtherFunction() {
+    myOtherVariable = 'local';
+    return myOtherVariable;
+}
+
+console.log(myVariable);
+console.log(myFunction());
+console.log(myOtherVariable);
+console.log(myOtherFunction());
+console.log(myOtherVariable);
