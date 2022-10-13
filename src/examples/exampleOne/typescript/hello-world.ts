7ca8b562419497dd03d50576785fb6ffaa3fa5de
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-13 17:07:37 -0300

    feat: adding step 28 - typescript: type inference | ES2015/ES6

--- src/examples/exampleOne/typescript/hello-world.ts
+++ src/examples/exampleOne/typescript/hello-world.ts
@@ -1,3 +1,20 @@
 let myName = 'Packt';
 // myName = 10;
 console.log(myName)
+
+let age: number = 20;
+let existsFlag: boolean = true;
+let language: string = 'JavaScript';
+
+// o TypeScript possui inferência de tipos, significa que o código verboso anterior pode ser reescrito da seguinte forma: 
+
+let age2 = 20; //number
+let existsFlag2 = true; //boolean
+let language2 = 'JavaScript'; //string
+
+// Porém, se declaramos a variável e não a inicializamos com um valor, então é necessário lhe atribuir um tipo:
+
+let favoriteLanguague: string; 
+let langs = ['JavaScript', 'Ruby', 'Python'];
+favoriteLanguague = langs[0];
+
