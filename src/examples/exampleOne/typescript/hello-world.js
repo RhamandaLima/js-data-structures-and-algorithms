4d82269bafe8abd1608c054a7675a90006f2a910
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-13 17:25:14 -0300

    feat: adding step 29 - typescript: interface | ES2015/ES6

--- src/examples/exampleOne/typescript/hello-world.js
+++ src/examples/exampleOne/typescript/hello-world.js
@@ -1,3 +1,23 @@
 var myName = 'Packt';
 // myName = 10;
 console.log(myName);
+var age = 20;
+var existsFlag = true;
+var language = 'JavaScript';
+// o TypeScript possui inferência de tipos, significa que o código verboso anterior pode ser reescrito da seguinte forma: 
+var age2 = 20; //number
+var existsFlag2 = true; //boolean
+var language2 = 'JavaScript'; //string
+// Porém, se declaramos a variável e não a inicializamos com um valor, então é necessário lhe atribuir um tipo:
+var favoriteLanguague;
+var langs = ['JavaScript', 'Ruby', 'Python'];
+favoriteLanguague = langs[0];
+console.log(favoriteLanguague);
+function printName(person) {
+    console.log(person.name);
+}
+var john = { name: 'john', age: 21 };
+var mary = { name: 'Mary', age: 21, phone: '123-45678' };
+printName(john);
+printName(mary);
+// b) Conceito 2: relacionado a POO; 
--- src/examples/exampleOne/typescript/hello-world.ts
+++ src/examples/exampleOne/typescript/hello-world.ts
@@ -17,4 +17,37 @@ let language2 = 'JavaScript'; //string
 let favoriteLanguague: string; 
 let langs = ['JavaScript', 'Ruby', 'Python'];
 favoriteLanguague = langs[0];
+console.log(favoriteLanguague)
 
+// Interface:
+
+// a) atribuição de um tipo a um variável. Conceito 1: uma descrição de atributos e métodos que um objeto deve ter: 
+interface Person {
+    name: string;
+    age: number;
+}
+
+function printName(person: Person) {
+    console.log(person.name);
+}
+
+const john = { name: 'john', age: 21 };
+const mary = { name: 'Mary', age: 21, phone: '123-45678'};
+printName(john);
+printName(mary);
+
+// b) Conceito 2: relacionado a POO
+
+interface Comparable {
+    compareTo(b): number;
+}
+
+class MyObject implements Comparable {
+    age: number;
+    compareTo(b): number {
+        if(this.age === b.age) {
+            return 0;
+        }
+        return this.age > b.age ? 1 : -1;
+    }
+}
