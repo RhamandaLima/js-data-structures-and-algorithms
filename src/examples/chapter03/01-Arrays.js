de264743b57d31c494113777be6d6547c943e8e6
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-17 21:15:28 -0300

    feat: adding step 31 - initializing arrays chapter

---
+++ src/examples/chapter03/01-Arrays.js
@@ -0,0 +1,15 @@
+const averageTempJan = 31.9;
+const averageTempFeb = 35.3;
+const averageTempMar = 42.4;
+const averageTempApr = 52;
+const averageTempMay = 60.8;
+
+// Com arrays:
+
+const averageTemp = [];
+averageTemp[0] = 31.9;
+averageTemp[1] = 35.3;
+averageTemp[2] = 42.4;
+averageTemp[3] = 52;
+averageTemp[4] = 60.8;
+console.log(averageTemp)
