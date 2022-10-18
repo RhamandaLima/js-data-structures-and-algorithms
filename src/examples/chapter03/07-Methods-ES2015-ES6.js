f812dbd22549f886b4c1254269a12e7e0dae50a1
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-18 11:38:08 -0300

    feat: adding step 46 - @@iterator | ES2015/ES6

--- src/examples/chapter03/07-Methods-ES2015-ES6.js
+++ src/examples/chapter03/07-Methods-ES2015-ES6.js
@@ -5,3 +5,19 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 for (const n of numbers) {
     console.log(`${n} é multiplo de 2:`, n % 2 === 0 ? 'even' : 'odd');
 }
+
+// Usando o objeto @@iterator:
+// Para usá-la, é necessário acessar a propriedade 'Symbol.iterator'. 
+
+let iterator = numbers[Symbol.iterator]();
+console.log(iterator.next().value); 
+console.log(iterator.next().value); 
+console.log(iterator.next().value); 
+console.log(iterator.next().value); 
+console.log(iterator.next().value); 
+
+// Para apresentar todos os valores do array com @@iterator: 
+
+for (const n of iterator) {
+    console.log(n);
+}
