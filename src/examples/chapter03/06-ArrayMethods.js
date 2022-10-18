34f9a50b610d7fa1f5ec89d1d5c3eec924c2c018
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-18 10:38:22 -0300

    feat: adding step 40 - joining arrays and iteration functions

---
+++ src/examples/chapter03/06-ArrayMethods.js
@@ -0,0 +1,26 @@
+// Juntando vários arrays: 'concat'
+
+const zero = 0; 
+
+const positiveNumbers = [1, 2, 3];
+console.log('Positive Numbers: ', positiveNumbers);
+
+const negativeNumbers = [-3, -2, -1];
+console.log('Negative Numbers: ', negativeNumbers);
+
+let numbers = negativeNumbers.concat(zero, positiveNumbers);
+console.log('Concat numbers: ', numbers);
+
+// Funções de iteração: 
+
+function isEven(x) {
+    console.log(x);
+    return x % 2 === 0 ? true : false;
+}
+let numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// Com a sintaxe ES2015(ES6): ArrowFunction
+
+const isEven1 = x => x % 2 === 0; 
+
+console.log('Numbers:', numbers1)
