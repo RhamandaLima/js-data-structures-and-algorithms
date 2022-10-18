db20dadd3615be2d9449426f4b120a2db8fedf05
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-18 15:50:07 -0300

    feat: adding step 48 - from | ES2015/ES6

--- src/examples/chapter03/07-Methods-ES2015-ES6.js
+++ src/examples/chapter03/07-Methods-ES2015-ES6.js
@@ -42,3 +42,12 @@ console.log(aKeys.next());
 // 'values':
 const aValues = numbers.values();
 console.log(aValues.next());
+
+// Usando o método 'from':
+// O método 'Array.from' cria outro arrat a partir de um array existente. 
+
+let numbers2 = Array.from(numbers);
+console.log(numbers2);
+
+let evens = Array.from(numbers, x => (x % 2 == 0));
+console.log(evens);
