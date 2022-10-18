bb5143310c94c2d88d48913430d34a43c7bcd798
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-18 10:04:29 -0300

    feat: adding step 39 - multi-dimensional array

--- src/examples/chapter03/05-TwoDimensionalMultiDimensionalArrays.js
+++ src/examples/chapter03/05-TwoDimensionalMultiDimensionalArrays.js
@@ -43,3 +43,27 @@ function printMatrix(myMatrix) {
 
 printMatrix(averageTemp);
 console.table(averageTemp);
+
+// Vamos criar uma matriz 3 x 3, isto é, um array multidimensional:
+// i = linha; j = coluna; z = profundidade.
+
+const matrix3x3x3 = [];
+for (let i = 0; i < 3; i++) {
+    matrix3x3x3[i] = [];
+    for (let j = 0; j < 3; j++) {
+        matrix3x3x3[i][j] = [];
+        for (let z = 0; z < 3; z++) {
+            matrix3x3x3[i][j][z] = i + j + z;            
+        }
+    }
+}
+
+for (let i = 0; i < matrix3x3x3.length; i++) {
+    for (let j = 0; j < matrix3x3x3[i].length; j++) {
+        for (let z = 0; z < matrix3x3x3[i][j].length; z++) {
+            console.log(matrix3x3x3[i][j][z])
+        }
+    }
+}
+
+console.table(matrix3x3x3);
