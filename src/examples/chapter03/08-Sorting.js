da12ff31ceae931a040ddbefe28b6a2a09769f16
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-18 20:44:05 -0300

    feat: adding step 53 - custom sorting | ES2015/ES6

--- src/examples/chapter03/08-Sorting.js
+++ src/examples/chapter03/08-Sorting.js
@@ -1,3 +1,5 @@
+// Ordenando elementos: 
+
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 numbers.reverse();
 console.log(numbers);
@@ -21,3 +23,22 @@ function compare(a, b) {
 }
 numbers.sort(compare);
 console.log(numbers);
+
+// Ordenação personalizada: 
+
+const friends = [
+    { name: 'John', age: 30 }, 
+    { name: 'Ana', age: 20 },
+    { name: 'Chris', age: 25 },
+]; 
+
+function comparePerson(a, b) {
+    if(a.age < b.age) {
+        return -1; 
+    }
+    if(a.age > b.age) {
+        return 1; 
+    }
+    return 0; 
+}
+console.log(friends.sort(comparePerson));
