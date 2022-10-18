c4347623e63bebd2b630ad6bfff8f031e4fbadc1
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-17 22:10:34 -0300

    feat: adding step 36 - removing elements on array

--- src/examples/chapter03/04-AddingRemovingElements.js
+++ src/examples/chapter03/04-AddingRemovingElements.js
@@ -29,3 +29,59 @@ console.log(numbers);
 numbers.unshift(-2);
 numbers.unshift(-4, -3);
 console.log(numbers);
+
+// Removendo um elemento do final do array: 
+
+numbers.pop();
+console.log(numbers);
+console.log(numbers.length);
+
+// Removendo um elemento da primeira posição: 
+
+for (let j = 0; j < numbers.length; j++) {
+    numbers[j] = numbers[j + 1];
+}
+console.log(numbers); // Com isso, o array permanece com o mesmo tamanho e a última posição atribui um valor 'undefined', ou seja, sobrecrevemos os valores originais e não o removemos de fato.
+
+Array.prototype.reIndex = function(myArray) {
+    const newArray = [];
+    for (let k = 0; k < myArray.length; k++) {
+        if (myArray[k] !== undefined) {
+            console.log(myArray[k]);
+            newArray.push(myArray[k]);
+        }
+    }
+    return newArray;
+};
+
+// remove a primeira posição manualmente e executa o reIndex: 
+
+Array.prototype.removeFirstPosition = function() {
+    for (let l = 0; l < this.length; l++) {
+        this[l] = this[l + 1];
+    }
+    return this.reIndex(this);
+};
+numbers = numbers.removeFirstPosition();
+console.log(numbers);
+
+// Contudo, a melhor prática é a utilização do método 'shift': 
+
+numbers.shift();
+console.log(numbers);
+
+// Adicionando e removendo elementos de uma posição específica
+// Com o splice, indicamos a posição/índice que a partir do qual queremos remover e quantidade de elementos:
+
+numbers.splice(5, 3);
+console.log(numbers);
+
+// OBS: Devemos sempre utilizar os métodos splice, pop ou shift para remover elementos. 
+
+// Supondo que queremos inserir os números 2 a 4 de volta ao array a partir da posição 5: 
+
+numbers.splice(5, 0, 2, 3, 4);
+console.log(numbers);
+
+numbers.splice(5, 3, 2, 3, 4);
+console.log(numbers);
