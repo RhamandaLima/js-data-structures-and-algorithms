a790ba131b00e2129fe91526e101c9c76fcac917
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-18 21:18:14 -0300

    feat: adding step 58 - toString and join | ES2015/ES6

---
+++ src/examples/chapter03/12-ArrayToString.js
@@ -0,0 +1,10 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// 'toString': 
+
+console.log(numbers.toString());
+
+// 'join': 
+
+const numbersString = numbers.join('-');
+console.log(numbersString);
